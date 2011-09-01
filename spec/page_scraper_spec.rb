ab964092884b70bc7d379ddb9defb209c3136313
David Tuite <dtuite@gmail.com>
2011-09-01 10:31:51 +0100

    now works fine with multiple fixtures

--- spec/page_scraper_spec.rb
+++ spec/page_scraper_spec.rb
@@ -1,16 +1,25 @@
+# TODO look at outputting which fixture caused the error by passing an error
+# message to rspec.should
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Page Scraper" do
   include DaftScraper
   before :all do
-    @fixture = Fixture.new("clareview-park-galway")
+    @fixtures = [ 
+      Fixture.new("clareview-park-galway"),
+      Fixture.new("belmaine-dublin-13")
+    ]
   end
 
   it "should scrape the price correctly" do
-    @fixture.extracted.price.should == @fixture.expected_price.to_i
+    @fixtures.each do |fixture|
+      fixture.extracted.price.should == fixture.expected_price.to_i
+    end
   end
 
   it "shoud return an integer price" do
-    @fixture.extracted.price.class.should == Fixnum
+    @fixtures.each do |fixture|
+      fixture.extracted.price.class.should == Fixnum
+    end
   end
 end
