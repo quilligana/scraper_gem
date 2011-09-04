fcb3346a6c744c443fe10187c7d31673bb70c761
David Tuite <dtuite@gmail.com>
2011-09-04 11:40:35 +0100

    just added a few comments

--- lib/daft/url.rb
+++ lib/daft/url.rb
@@ -5,6 +5,8 @@ module Daft
       @url = url
     end
 
+    # TODO check that it is actually a url from the daft website
+    # rather than just a URL from anywhere on the internet
     def valid?
       true unless (url =~ URI::regexp).nil?
     end
@@ -15,6 +17,14 @@ module Daft
 
     def id
       url.match(/id=\d+/)[0].gsub(/\D/, '').to_i
+    end
+
+    # IDEA I kinda think that this pattern makes the PageScraper class
+    # one abstraction too many. Should maybe move the logic from that class
+    # into this method
+    # TODO: deal with Mechanize errors
+    def scrape
+      PageScraper.new(self.to_s).listing
     end
   end
 end
