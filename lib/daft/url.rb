2add0be91afa35f6123142b13ace37ebbe6d583b
David Tuite <dtuite@gmail.com>
2011-09-04 11:23:46 +0100

    basic validation test working

--- lib/daft/url.rb
+++ lib/daft/url.rb
@@ -1,6 +1,12 @@
 module Daft
   class Url
+    attr_accessor :url
+    def initialize url
+      @url = url
+    end
 
-
+    def valid?
+      true unless (url =~ URI::regexp).nil?
+    end
   end
 end
--- spec/daft/url_spec.rb
+++ spec/daft/url_spec.rb
@@ -1,5 +1,16 @@
 require "spec_helper"
 
 describe Daft::Url do
-  
+  include Daft
+  describe "valid?" do
+    it "should return true if the url is valid" do
+      url = "http://www.daft.ie/searchrental.daft?id=123456"
+      Url.new(url).should be_valid
+    end
+
+    it "should return true if the url is valid" do
+      url = "Totally invalid URL"
+      Url.new(url).should_not be_valid
+    end
+  end
 end
